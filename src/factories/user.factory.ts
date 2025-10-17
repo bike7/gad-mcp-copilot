19811ab7356473f7e10d1ea8bfb593ca094b2454
Katarzyna Pytel <pytel.kat@gmail.com>
2025-10-17 16:06:07 +0200

    refactor: formatting

--- src/factories/user.factory.ts
+++ src/factories/user.factory.ts
@@ -4,15 +4,19 @@ export class UserFactory {
   static createTestUser(): User {
     const firstName = faker.person.firstName().replace(/[^A-Za-z]/g, "");
     const lastName = faker.person.lastName().replace(/[^A-Za-z]/g, "");
+    const email = faker.internet.email();
+    const password = `${faker.string.alphanumeric(8)}A1!`;
+    const birthDate = faker.date
+      .birthdate({ min: 18, max: 65, mode: "age" })
+      .toISOString()
+      .split("T")[0];
+
     return {
       firstName,
       lastName,
-      email: faker.internet.email(),
-      birthDate: faker.date
-        .birthdate({ min: 18, max: 65, mode: "age" })
-        .toISOString()
-        .split("T")[0],
-      password: `${faker.string.alphanumeric(8)}A1!`,
+      email,
+      birthDate,
+      password,
     };
   }
 }
