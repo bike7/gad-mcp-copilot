fc2640f1dc48d384d774e770bda7c0fc688b0d06
Katarzyna Pytel <pytel.kat@gmail.com>
2026-01-28 11:24:39 +0100

    update thresholds

--- tests/nonfunctional/performance.audit.spec.ts
+++ tests/nonfunctional/performance.audit.spec.ts
@@ -10,8 +10,8 @@ test.describe('Lighthouse Performance Audit', () => {
   test('Home page should meet performance standards', async ({ page }) => {
     //Arrange
     const thresholds = {
-      performance: 60,
-      accessibility: 60,
+      performance: 50,
+      accessibility: 50,
       bestPractices: 100,
       seo: 50,
     };
@@ -56,8 +56,8 @@ test.describe('Lighthouse Performance Audit', () => {
 
   test('Register page should meet performance standards', async ({ page }) => {
     const thresholds = {
-      performance: 60,
-      accessibility: 60,
+      performance: 50,
+      accessibility: 50,
       bestPractices: 70,
       seo: 50,
     };
